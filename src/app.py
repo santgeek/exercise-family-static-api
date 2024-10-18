52278df58fde622420e6cd4834a6d31e1c65b972
Santiago Montoya <santiago.mordz@gmail.com>
2024-10-18 19:55:06 +0000

    Python endpoints

--- src/app.py
+++ src/app.py
@@ -14,6 +14,33 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+# velazques_family = FamilyStructure("Velaszquez")
+
+John = {
+    "first_name": "John",
+    "last_name": jackson_family.last_name,
+    "age": 33,
+    "lucky numbers": [7, 13, 22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "last_name": jackson_family.last_name,
+    "age": 35,
+    "lucky numbers": [10, 14, 3]
+} 
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "last_name": jackson_family.last_name,
+    "age": 5,
+    "lucky numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,17 +53,37 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+    
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({'message': 'Miembro no encontrado'}), 404
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def post_member():
+    member = request.json    
+    if jackson_family.add_member(member):  
+        return jsonify(member), 201  
+    else:  
+        return jsonify({'message': 'Error al crear el miembro'}), 400 
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_element(id):
+    member = jackson_family.get_member(id)
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message": f"Miembro eliminado: {member}"}), 200
+    else:
+        return jsonify({'message': 'Error al eliminar'}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
